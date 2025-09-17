87bab95ceff837b9b11c15c4944bec762b525653
Tyron <tyronodame5@gmail.com>
2025-09-17 12:48:14 -0400

    refactor: improve variable declaration and formatting in playground.go

--- GoLang/playground.go
+++ GoLang/playground.go
@@ -1,12 +1,21 @@
 package main
 
 import "fmt"
+// this is an example of a global variable
+var ex string = "Global string Example"
+
+
+
 
 
 func adding(a int, b int) {
 	fmt.Println(a + b)
 }
+
 func main() {
+	name := "tyron"
+
 	fmt.Println("Hello, Go Playground!")
+	fmt.Println(name)
 	adding(5, 10)
 }
