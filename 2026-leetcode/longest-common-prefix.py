b1537ace86c8d4093e1591f67fded8da39d76ee7
Tyron <tyronodame5@gmail.com>
2026-01-27 23:43:05 -0500

    feat: implement longestCommonPrefix function to return the longest common prefix of an array of strings

---
+++ 2026-leetcode/longest-common-prefix.py
@@ -0,0 +1,38 @@
+"""
+Docstring for 2026-leetcode.longest-common-prefix
+
+
+You are given an array of strings strs. Return the longest common prefix of all the strings.
+
+If there is no longest common prefix, return an empty string "".
+
+Example 1:
+
+Input: strs = ["bat","bag","bank","band"]
+
+Output: "ba"
+Example 2:
+
+Input: strs = ["dance","dag","danger","damage"]
+
+Output: "da"
+Example 3:
+
+Input: strs = ["neet","feet"]
+
+Output: ""
+
+"""
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        prefix = strs[0]
+        for i in range(1, len(strs)):
+            j = 0
+            while j < min(len(prefix), len(strs[i])):
+                if prefix[j] != strs[i][j]:
+                    break
+                j += 1
+            prefix = prefix[:j]
+        return prefix
